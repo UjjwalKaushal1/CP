624b202650fc6ae32471187b48a1dcd2d6387f5f
ujjwal kaushal <ujjwalk0204@gmail.com>
2024-08-11 23:18:28 +0530

    Added new file

---
+++ AtCoder/Welcome_AtCoder.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+#define ll long long
+#define v(ll) vector<long long>
+#define v(i) vector<int>
+#define v(c) vector<char>
+#define pb push_back
+#define po pop_back
+#define sort(all) sort(v.begin(),v.end())
+#define rev(all) reverse(v.begin(),v.end())
+*/
+
+int main()
+{
+    int a,b,c;
+    cin >> a;
+    cin >> b >> c;
+    string s;
+    cin >> s;
+
+    int res = a+b+c;
+    cout << res << " " << s;
+    
+}
