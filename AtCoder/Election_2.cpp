ea24938a138ffd51a730202e6167629da78ba6db
ujjwal kaushal <ujjwalk0204@gmail.com>
2024-08-11 23:42:11 +0530

    Added new file

---
+++ AtCoder/Election_2.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+#define ll long long
+#define v(ll) vector<long long>
+#define v(i) vector<int>
+#define v(c) vector<char>
+#define pb push_back
+#define po pop_back
+#define sort(all) sort(v.begin(),v.end())
+#define rev(all) reverse(v.begin(),v.end())
+*/
+
+int main()
+{
+    int n,t,a;
+    cin >> n >> t >> a;
+
+    int half = n/2;
+    if(t > half || a > half)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+    
+}
