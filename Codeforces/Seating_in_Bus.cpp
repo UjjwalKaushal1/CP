3d30073fc8d0ea11aa80bc4eaff5cf3f346fca3b
ujjwal kaushal <ujjwalk0204@gmail.com>
2024-08-13 21:00:10 +0530

    Added new file

--- Codeforces/Seating_in_Bus.cpp
+++ Codeforces/Seating_in_Bus.cpp
@@ -1,20 +1,43 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
 using namespace std;
-int main()
 
-{
+int main() {
     int t;
     cin >> t;
-    while(t--)
-    {
+
+    while (t--) {
         int n;
         cin >> n;
 
-        vector<int> v(n);
-        for(int i = 0; i<n; i++)
-        {
-            cin >> v[i];
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
         }
-        
+
+        unordered_set<int> occupied;
+        bool valid = true;
+
+        occupied.insert(a[0]); 
+
+        for (int i = 1; i < n; ++i) {
+            int seat = a[i];
+
+            if (occupied.count(seat - 1) == 0 && occupied.count(seat + 1) == 0) {
+                valid = false;
+                break;
+            }
+
+            occupied.insert(seat); 
+        }
+
+        if (valid) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
+
+    return 0;
 }
